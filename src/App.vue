0e210fe63687d4e95da94d0d62f9053fc173140e
= <rballshahbazi2@gmail.com>
2021-12-30 22:52:24 +0330

    draw cubes, but its messy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-stage ref="stage" :config="stageConfig" @click="handleStageClick">
+      <v-layer>
+        <v-cube v-for="cube in cubes" :key="cube.id" :config="cube"></v-cube>
+      </v-layer>
+    </v-stage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import VCube from "./components/VCube";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {VCube},
+  data() {
+    return {
+      clickCount: 0,
+      stageConfig: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+      cubeConfig: {},
+      cubes: [],
+    }
+  },
+  methods: {
+    handleStageClick() {
+      const {x, y} = this.$refs.stage.getNode().getPointerPosition()
+
+      // reset click count after every three clicks
+      this.clickCount = ++this.clickCount <= 3 ? this.clickCount : 1
+      this.cubeConfig[`click${this.clickCount}`] = {
+        x,
+        y
+      }
+
+      if (this.clickCount === 2) {
+        this.drawCube(x, y)
+      }
+
+      if (this.clickCount === 3) {
+        this.updateCube()
+      }
+    }
+    ,
+    drawCube(x, y) {
+      const prevX = this.cubeConfig['click1'].x
+      const prevY = this.cubeConfig['click1'].y
+      this.cubes.push({
+        front: {
+          id: (Math.random() + 1).toString(36).substring(7),
+          x: prevX,
+          y: prevY,
+          width: x - prevX,
+          height: y - prevY,
+          fill: '#f00',
+        },
+        cubeConfig: this.cubeConfig,
+      })
+    }
+    ,
+    updateCube() {
+      const lastCube = this.cubes.pop()
+      const topPoints = this.createTopFace(lastCube)
+      const sidePoints = this.createSideFace(lastCube)
+      this.cubes.push({
+        ...lastCube,
+        topFace: {
+          points: topPoints,
+          fill: '#a00',
+          closed: true,
+        },
+        sideFace: {
+          points: sidePoints,
+          fill: '#800',
+          closed: true,
+        }
+      })
+    },
+    createTopFace(cube) {
+      // const topFace = {}
+      // const sideFace = {}
+      const points = []
+      points.push(cube.cubeConfig['click3'].x)
+      points.push(cube.cubeConfig['click3'].y)
+      points.push(cube.cubeConfig['click3'].x - cube.front.width)
+      points.push(cube.cubeConfig['click3'].y)
+      points.push(cube.cubeConfig['click1'].x)
+      points.push(cube.cubeConfig['click1'].y)
+      points.push(cube.cubeConfig['click1'].x + cube.front.width)
+      points.push(cube.cubeConfig['click1'].y)
+      return points
+    },
+    createSideFace(cube) {
+      const points = []
+      points.push(cube.cubeConfig['click3'].x)
+      points.push(cube.cubeConfig['click3'].y)
+      points.push(cube.cubeConfig['click1'].x + cube.front.width)
+      points.push(cube.cubeConfig['click1'].y)
+      points.push(cube.cubeConfig['click2'].x)
+      points.push(cube.cubeConfig['click2'].y)
+      points.push(cube.cubeConfig['click3'].x)
+      points.push(cube.front.height + cube.cubeConfig['click3'].y)
+      return points
+    },
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin: 0;
+  box-sizing: border-box;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/VCube.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-group>
+      <v-rect :config="config.front"></v-rect>
+      <v-line :config="config.topFace"></v-line>
+      <v-line :config="config.sideFace"></v-line>
+    </v-group>
+</template>
+
+<script>
+export default {
+  name: "VCube",
+  props: {
+    config: {
+      type: Object,
+      required: true,
+    }
+  },
+  watch: {
+    config: {
+      handler(newConfig) {
+        console.log(newConfig)
+      },
+      deep: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
